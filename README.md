0c821ed29882888476d5ca5c282ba1e1ebf6c66c
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-06-26 01:50:03 +0530

    chore: refactor vercel deployment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ineuron-chat-bot
+
+## Production
+
+- Use Gunicorn `gunicorn --bind 0.0.0.0:$PORT --reload wsgi:app`
--- manage.py
+++ manage.py
@@ -1,7 +1,3 @@
 from application import create_app
 
-
 app = create_app()
-
-if __name__ == "__main__":
-    app.run()

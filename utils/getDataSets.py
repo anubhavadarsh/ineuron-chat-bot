9031cc6858d9e27f930e14fa68937c1cb58e67b7
anubhavadarsh <anubhav.adarsh9@gmail.com>
2022-06-26 07:35:45 +0530

    add: add heavy and medium data set

--- utils/getDataSets.py
+++ utils/getDataSets.py
@@ -2,4 +2,6 @@ def getDataSets(dataSetName):
     if dataSetName == "light":
         return f"./data/dat_light.csv"
     elif dataSetName == "medium":
-        return f"./data/dat2.csv"
+        return f"./data/dat_medium.csv"
+    elif dataSetName == "heavy":
+        return f"./data/dat_heavy.csv"
